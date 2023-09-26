0cd894170de76fd085cd13c13718fa1423b2aa80
Hüseyin Aydın <huseyinaydin99@gmail.com>
2023-09-26 12:12:38 +0300

    Update EmployeeBuilder.java

--- src/main/java/com/huseyinaydin/util/EmployeeBuilder.java
+++ src/main/java/com/huseyinaydin/util/EmployeeBuilder.java
@@ -9,20 +9,20 @@ import com.huseyinaydin.entity.Employee;
 public class EmployeeBuilder {
 
     public static List<EmployeeDTO> getListDTO() {
-        return Arrays.asList(new EmployeeDTO(1,"Basant","DEV",50000),
-                new EmployeeDTO(2,"Santosh","QA",80000));
+        return Arrays.asList(new EmployeeDTO(1,"Huseyin","DEV",50000),
+                new EmployeeDTO(2,"Niğde","QA",80000));
     }
 
     public static List<Employee> getListEntities() {
-        return Arrays.asList(new Employee(1,"Basant","DEV",50000),
-                new Employee(2,"Santosh","QA",80000));
+        return Arrays.asList(new Employee(1,"Huseyin","DEV",50000),
+                new Employee(2,"Niğde","QA",80000));
     }
 
     public static EmployeeDTO getDTO() {
-        return new EmployeeDTO(1,"Basant","DEV",50000);
+        return new EmployeeDTO(1,"Huseyin","DEV",50000);
     }
 
     public static Employee getEntity() {
-        return new Employee(1,"Basant","DEV",50000);
+        return new Employee(1,"Huseyin","DEV",50000);
     }
 }
